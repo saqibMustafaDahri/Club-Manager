d3ae8afff371c7af459512b258d19adf6463fcae
Saqib Mustafa Dahri <saqibmustafa753@gmail.com>
2026-06-15 11:22:13 +0500

    ddddd

--- api/index.js
+++ api/index.js
@@ -1,17 +1,33 @@
-import server from '../dist/server/index.mjs'
+// import server from '../dist/server/index.mjs'
 
-export default async function handler(req, res) {
+// export default async function handler(req, res) {
+//   const url = `https://${req.headers.host}${req.url}`
+//   const request = new Request(url, {
+//     method: req.method,
+//     headers: req.headers,
+//   })
+
+//   const response = await server.fetch(request)
+
+//   res.statusCode = response.status
+//   response.headers.forEach((value, key) => res.setHeader(key, value))
+
+//   const body = await response.text()
+//   res.end(body)
+// }
+import handler from '../dist/server/server.js'
+
+export default async function (req, res) {
   const url = `https://${req.headers.host}${req.url}`
   const request = new Request(url, {
     method: req.method,
     headers: req.headers,
   })
 
-  const response = await server.fetch(request)
+  const response = await handler.fetch(request)
 
   res.statusCode = response.status
   response.headers.forEach((value, key) => res.setHeader(key, value))
-
   const body = await response.text()
   res.end(body)
 }
