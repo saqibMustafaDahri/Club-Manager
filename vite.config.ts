79b9a172d29ea0b2b5d1c3f4a0a4bf80dbb332d4
Saqib Mustafa Dahri <saqibmustafa753@gmail.com>
2026-06-15 12:23:03 +0500

    new-chanages are made

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { tanstackStart as TanStackStartVite } from '@tanstack/react-start/plugin/vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  nitro: true,
-  tanstackStart: {
-    router: {
-      autoCodeSplitting: false,
-      codeSplittingOptions: {
-        defaultBehavior: [],
-      },
-    },
-  },
-});
+  plugins: [
+    TanStackStartVite(),
+    react(),
+    tailwindcss(),
+    tsConfigPaths(),
+  ],
+})
