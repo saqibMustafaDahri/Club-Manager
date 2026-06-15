b8c14de0441895f0464b4ec811a6496988735be1
Saqib Mustafa Dahri <saqibmustafa753@gmail.com>
2026-06-15 11:44:48 +0500

    change in files

--- api/index.js
+++ api/index.js
@@ -15,17 +15,15 @@
 //   const body = await response.text()
 //   res.end(body)
 // }
-import handler from '../dist/server/server.js'
+import handler from './server.js'
 
 export default async function (req, res) {
-  const url = `https://${req.headers.host}${req.url}`
+  const url = 'https://${req.headers.host}${req.url}'
   const request = new Request(url, {
     method: req.method,
     headers: req.headers,
   })
-
   const response = await handler.fetch(request)
-
   res.statusCode = response.status
   response.headers.forEach((value, key) => res.setHeader(key, value))
   const body = await response.text()
